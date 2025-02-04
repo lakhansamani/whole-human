2e361b3315ae1d594c25ca51145f38ffe482df4c
Lakhan Samani <lakhan.m.samani@gmail.com>
2025-02-04 11:37:13 +0530

    chore: update logo and meta text

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,35 +12,41 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <title>Whole Human - Wellness Hub</title>
+        <title>Whole Human - Social Wellness</title>
         <meta
           name='description'
           content='Whole Human is dedicated to promoting holistic wellbeing, sustainability, inclusion, equality, and personal growth. Join us in our journey towards a better future.'
         />
         <meta
           name='keywords'
-          content='Holistic Wellbeing, Sustainability, Inclusion, Equality, Growth Mindset, Wellness, Whole Human, Personal Development'
+          content='Holistic Wellbeing, Sustainability, Inclusion, Equality, Growth Mindset, Wellness, Whole Human, Personal Development, Social'
         />
         <meta name='author' content='Whole Human' />
         <meta name='robots' content='index, follow' />
         <link rel='icon' href='/assets/images/favicon.png' sizes='any' />
-        <meta property='og:title' content='Whole Human - Wellness Hub' />
+        <meta property='og:title' content='Whole Human - Social Wellness' />
         <meta
           property='og:description'
           content='Promoting holistic wellbeing, sustainability, inclusion, equality, and personal growth.'
         />
-        <meta property='og:image' content='/assets/images/logo.png' />
+        <meta
+          property='og:image'
+          content='/assets/images/whole-human-logo.png'
+        />
         <meta property='og:url' content='https://www.wholehuman.com' />
         <meta property='og:type' content='website' />
 
         {/* Twitter */}
         <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content='Whole Human - Wellness Hub' />
+        <meta name='twitter:title' content='Whole Human - Social Wellness' />
         <meta
           name='twitter:description'
           content='Join us in promoting holistic wellbeing, sustainability, inclusion, equality, and personal growth.'
         />
-        <meta name='twitter:image' content='/assets/images/logo.png' />
+        <meta
+          name='twitter:image'
+          content='/assets/images/whole-human-logo.png'
+        />
       </head>
       <body className={montserrat.className}>
         {children}
